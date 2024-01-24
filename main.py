f2ece7e3ef3ab8fea9033b707efc149245652cd6
vignesh <vignesh2914@gmail.com>
2024-01-24 11:31:13 +0530

    Updated to 24 hr jobs filter

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-file_path = 'linkedin-jobs-c++.csv'
+file_path = 'linkedin-jobs-python_developer.csv'
 
 def linkedin_scraper(webpage, keywords, page_number):
     next_page = f"{webpage}&keywords={keywords}&start={page_number}"
@@ -21,13 +21,12 @@ def linkedin_scraper(webpage, keywords, page_number):
     for job in jobs:
         job_title = job.find('h3', class_='base-search-card__title').text.strip()
         job_company = job.find('h4', class_='base-search-card__subtitle').text.strip()
-        job_location = job.find('span', class_='job-search-card__location').text.strip()
+        job_location = job.find('span', class_='job-search-card__location').text.strip()  
         job_link = job.find('a', class_='base-card__full-link')['href']
 
         with open(file_path, 'a', encoding='utf-8', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow([job_title, job_company, job_location, job_link])
-
+            writer.writerow([job_title, job_company, job_location, job_link]) 
     print('Data updated')
 
     if page_number < 25:
@@ -36,10 +35,7 @@ def linkedin_scraper(webpage, keywords, page_number):
     else:
         print('File closed')
 
-user_input = input("Enter the job keywords: ")
+user_keywords = input("Enter the job keywords: ")
 
-
-dynamic_url = f'https://www.linkedin.com/jobs/search/?keywords={user_input}&location=India&origin=JOB_SEARCH_PAGE_SEARCH_BUTTON&refresh=true'
-
-linkedin_scraper(dynamic_url, user_input, 0)
-
+dynamic_url = f'https://www.linkedin.com/jobs/search/?currentJobId=3808377479&distance=25&f_TPR=r86400&geoId=102713980&keywords={user_keywords}&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true'
+linkedin_scraper(dynamic_url, user_keywords, page_number=0)
